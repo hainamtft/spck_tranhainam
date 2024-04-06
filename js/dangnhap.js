56fc2a832fc960da12b6386541b5d35534095b91
hainam <155862587+hainamtft@users.noreply.github.com>
2024-04-06 23:43:28 +0700

    update

---
+++ js/dangnhap.js
@@ -0,0 +1,23 @@
+var login = document.getElementById("button");
+login.addEventListener("click",getvalue);
+function getvalue(){
+    let name = document.getElementById("name").value;
+    let gmail = document.getElementById("gmail").value;
+    let password = document.getElementById("password").value;
+    if ( name === "" && gmail === "" && password === ""){
+        alert("vui lòng điền thông tin")
+    }
+    else if (name === ""){
+        alert("vui lòng điền tên")
+    }
+    else if(gmail === ""){
+        alert("vui lòng điền gmail")
+    }
+    else if(password === ""){
+        alert("vui lòng điền password")
+    }
+    else {
+        alert("đăng nhập thành công");
+        window.location.href ="../index.html";
+    }
+}
